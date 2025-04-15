e75cfe004741bb3d9876949c0913ae25a05551e3
habiba-07 <bobkhalil704@gmail.com>
2025-04-15 23:06:57 +0200

    added LCD driver part 1

---
+++ LCD/LCD_config.h
@@ -0,0 +1,27 @@
+/****************************************************************/
+/******* Author    : Habiba Ahmed               *****************/
+/******* Date      : 14/4/2025                  *****************/
+/******* File Name : LCD_config.h            *****************/
+/****************************************************************/
+
+#ifndef LCD_CONFIG_H_
+#define LCD_CONFIG_H_
+
+#define LCD_CONTROL_PORT    PortA
+#define LCD_DATA_PORT       PortC
+
+#define LCD_RS_PIN      Pin0
+#define LCD_RW_PIN      Pin1
+#define LCD_E_PIN       Pin2
+
+#define LCD_D0_PIN   Pin0
+#define LCD_D1_PIN   Pin1
+#define LCD_D2_PIN   Pin2
+#define LCD_D3_PIN   Pin3
+#define LCD_D4_PIN   Pin4
+#define LCD_D5_PIN   Pin5
+#define LCD_D6_PIN   Pin6
+#define LCD_D7_PIN   Pin7
+
+
+#endif
