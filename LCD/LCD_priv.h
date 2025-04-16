4fa02d41feed8349293251a038ad50d4a186fb98
habiba-07 <138677249+habiba-07@users.noreply.github.com>
2025-04-16 22:50:47 +0200

    Update LCD_priv.h
    there was two LCD_CLR macros i removed one of them

--- LCD/LCD_priv.h
+++ LCD/LCD_priv.h
@@ -14,7 +14,6 @@ static void LCD_voidSendData(u8 data);
 #define LCD_HOME                 0x02
 #define LCD_SHIFT_RIGHT          0X1C
 #define LCD_SHIFT_LEFT           0X18
-#define LCD_CLR                  0x01 
 #define LCD_FUN_SET_TWOLINE_5x7  0x38
 #define LCD_DIS_ON_SETUP         0x0F
 #define LCD_SHIFT_INC            0x06
