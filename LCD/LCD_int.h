792aa298a954eca061d01ae8ba8f1ec20c4e559f
Bassel Mahmoud <basselmahmoud1@gmail.com>
2025-04-19 14:57:36 +0200

    adding new files to work on

---
+++ LCD/LCD_int.h
@@ -0,0 +1,16 @@
+/****************************************************************/
+/******* Author    : Habiba Ahmed               *****************/
+/******* Date      : 15/4/2025                  *****************/
+/******* File Name : LCD_int.h            *****************/
+/****************************************************************/
+#ifndef LCD_INT_H_
+#define LCD_INT_H_
+
+void LCD_voidInit(void);
+void LCD_voidSendCommand(u8 command);
+void LCD_voidSendString(const u8 *Str);
+void LCD_voidMoveCursor(u8 row,u8 col);
+void LCD_voidClearScreen(void);
+
+void Delay(u32 milisec);
+#endif
---
+++ LCD/LCD_priv.h
@@ -0,0 +1,23 @@
+/****************************************************************/
+/******* Author    : Habiba Ahmed               *****************/
+/******* Date      : 15/4/2025                  *****************/
+/******* File Name : LCD_priv.h            *****************/
+/****************************************************************/
+
+#ifndef LCD_PRIV_H_
+#define LCD_PRIV_H_
+
+static void LCD_voidSendEnablePulse();
+static void LCD_voidSendData(u8 data);
+
+#define LCD_CLR                  0x01
+#define LCD_HOME                 0x02
+#define LCD_SHIFT_RIGHT          0X1C
+#define LCD_SHIFT_LEFT           0X18
+#define LCD_FUN_SET_TWOLINE_5x7  0x38
+#define LCD_DIS_ON_SETUP         0x0F
+#define LCD_SHIFT_INC            0x06
+#define LCD_RETURN_HOME          0x02
+
+
+#endif
