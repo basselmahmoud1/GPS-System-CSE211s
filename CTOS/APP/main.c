60a97b7d73e2b1a212b6817520e90643a3f7931c
Maya Ahmed <mmayaahmed32@gmail.com>
2025-04-20 22:47:26 +0200

    testbench

--- CTOS/APP/main.c
+++ CTOS/APP/main.c
@@ -11,36 +11,82 @@ void Delay(u32 millisec)
 }
 
 int main(void){
+		u8 PinValue_sw1,PinValue_sw2;
 		u8 PinValue;
-		u8 result;
-    GPIO_u8ConfigPORT(PortA,GPIO_OUTPUT,0xFF,0);
-    GPIO_u8ConfigPin(PortB,Pin3,GPIO_INPUT,0,GPIO_PD);
-    GPIO_u8ConfigPin(PortB,Pin2,GPIO_INPUT,0,GPIO_PU);
-    GPIO_u8ConfigPin(PortC,Pin0,GPIO_OUTPUT,GPIO_OUT_HIGH,0);
-    GPIO_u8ConfigPin(PortC,Pin1,GPIO_OUTPUT,GPIO_OUT_LOW,0);
-		GPIO_u8SetMode(PortB,Pin0,PB0_U1Rx);
+		u8 sw_1,sw_2,value;
+	//configure leds as output ,initially low
+		GPIO_u8ConfigPin(PortF,Pin1,GPIO_OUTPUT,GPIO_OUT_LOW,0);
+		GPIO_u8ConfigPin(PortF,Pin2,GPIO_OUTPUT,GPIO_OUT_LOW,0);
+		GPIO_u8ConfigPin(PortF,Pin3,GPIO_OUTPUT,GPIO_OUT_LOW,0);
+	//configure swithces as input
+		GPIO_u8ConfigPin(PortF,Pin0,GPIO_INPUT,0,GPIO_PU);
+		GPIO_u8ConfigPin(PortF,Pin4,GPIO_INPUT,0,GPIO_PU);
+	//set portb as output and portd as input
+		GPIO_u8ConfigPORT(PortB,GPIO_OUTPUT,0x0F,0);
+		GPIO_u8ConfigPORT(PortD,GPIO_INPUT,0,GPIO_PD);
+	//testing setmode function
+		GPIO_u8SetMode(PortA,Pin0,PA0_U0Rx);
+		GPIO_u8SetMode(PortA,Pin1,PA1_U0Tx);
+
+
     while(1)
     {
-        GPIO_u8SetPortValue(PortA,GPIO_OUT_HIGH);
-        Delay(1000);
-        GPIO_u8SetPinValue(PortA,Pin1,GPIO_OUT_LOW);
-        Delay(1000);
-        //Test for GetPinValue
 
-        result=GPIO_u8GetPinValue(PortA,Pin1,&PinValue);
+        sw_1=GPIO_u8GetPinValue(PortF,Pin0,&PinValue_sw1);
+        sw_2=GPIO_u8GetPinValue(PortF,Pin4,&PinValue_sw2);
 
-        if (result==NOERR)  // Check if the read was successful
-        {
-            if(PinValue==1)
-                printf("Pin A1 is HIGH\n");
-            else
-                printf("Pin A1 is LOW\n");
-        }
-        else
-            printf("Error reading pin!\n"); //ERROR
-    }
-    //Testing
+        if (sw_1==NOERR&&PinValue_sw1==0){ //if switch1 is pressed , light red
+            GPIO_u8SetPinValue(PortF,Pin1,GPIO_OUT_HIGH);
+						Delay(1000);
+						GPIO_u8SetPinValue(PortF,Pin1,GPIO_OUT_LOW); }
+
+        if (sw_2==NOERR&&PinValue_sw2==0){// if switch2 is pressed , light blue
+            GPIO_u8SetPinValue(PortF,Pin2,GPIO_OUT_HIGH);
+						Delay(1000);
+						GPIO_u8SetPinValue(PortF,Pin2,GPIO_OUT_LOW);}
+
+
+			if(sw_1==NOERR&&sw_2==NOERR)     // if both swithces pressed ,light green
+			{
+						if(!(PinValue_sw1&&PinValue_sw2))
+					   GPIO_u8SetPinValue(PortF,Pin3,GPIO_OUT_HIGH);
+						Delay(1000);
+						GPIO_u8SetPinValue(PortF,Pin3,GPIO_OUT_LOW);
+						}
+
+			GPIO_u8SetPinValue(PortB,Pin1,GPIO_OUT_LOW); //changing PortB pin1 from high to low
+			Delay(1000);
+			value=GPIO_u8GetPinValue(PortB,Pin1,&PinValue);
+			if(value==NOERR&&PinValue==0){
+				  GPIO_u8SetPinValue(PortF,Pin0,GPIO_OUT_LOW); //light red if PortB pin1 changed from high to low
+					Delay(1000);
+					GPIO_u8SetPinValue(PortF,Pin0,GPIO_OUT_LOW);}
+
+			//testing PORTD pins
+			value=GPIO_u8GetPinValue(PortD,Pin0,&PinValue);
+			if(value==NOERR&&PinValue==1){
+				  GPIO_u8SetPinValue(PortF,Pin0,GPIO_OUT_LOW); //light red if portd pin0 is high
+					Delay(1000);
+					GPIO_u8SetPinValue(PortF,Pin0,GPIO_OUT_LOW);}
+
+			value=GPIO_u8GetPinValue(PortD,Pin1,&PinValue);
+			if(value==NOERR&&PinValue==1){
+				  GPIO_u8SetPinValue(PortF,Pin1,GPIO_OUT_LOW); //light blue if portd pin1 is high
+					Delay(1000);
+					GPIO_u8SetPinValue(PortF,Pin1,GPIO_OUT_LOW);}
+
+			value=GPIO_u8GetPinValue(PortD,Pin5,&PinValue);
+			if(value==NOERR&&PinValue==1)
+				  GPIO_u8SetPinValue(PortF,Pin2,GPIO_OUT_LOW); //light green if PortD pin5 is high
+					Delay(1000);
+					GPIO_u8SetPinValue(PortF,Pin2,GPIO_OUT_LOW);}
+
+
+
+
+
 
 
 }
+
 
